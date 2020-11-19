16f1c82610219709ad637570cbe5a0d6be359269
Matt Magin <matthew.d.magin@gmail.com>
2020-11-19 18:40:37 +1000

    Added dynamicReplaceMulti and dynamicReplaceSingle
    Both function used heavily in some accounts. About time to get it in the fold.
    
    Known Bug: dynamicReplaceMulti(), when inputValue is blank it injects infinite spans rather than just one.

--- js/formatters.js
+++ js/formatters.js
@@ -1,9 +1,56 @@
 function removeWord() {
   let elements = document.querySelectorAll("[data-remove-word]");
-  elements.forEach(el => {
+  elements.forEach((el) => {
     let wordToRemove = el.dataset.removeWord;
     let text = el.innerHTML;
     var res = text.replaceAll(wordToRemove, "");
     el.innerHTML = res;
+  });
+}
+
+function dynamicReplace(selector = null, data = null) {
+  if (selector != null && data != null) dynamicReplaceMulti(selector, data);
+  dynamicReplaceSingle();
+}
+
+function dynamicReplaceMulti(target, data) {
+  const targets = document.querySelectorAll(target);
+  if (targets.length === 0) return;
+
+  targets.forEach(function (target) {
+    data.forEach(function (item) {
+      const inputValue = item[0];
+      const lookup = item[1];
+
+      if (
+        target.innerText.includes(lookup) != -1 &&
+        (inputValue != "" || inputValue.length != 0)
+      ) {
+        target.innerHTML = target.innerHTML.replaceAll(lookup, inputValue); //If found replace
+      } else {
+        //NEED TO FIX
+        let lookupSpan = `<span class='lookup-value'>${lookup}</span>`;
+        console.log(target.innerHTML);
+        let temp = target.innerHTML.split(lookupSpan);
+        temp = temp.map((x) => x.replaceAll(lookup, lookupSpan));
+        target.innerHTML = temp.join(lookupSpan);
+      }
+    });
+  });
+}
+
+function dynamicReplaceSingle() {
+  const elements = document.querySelectorAll("[data-replace-from]");
+  if (elements.length === 0) return;
+
+  elements.forEach((el) => {
+    const text = el.innerText;
+    const html = el.innerHTML;
+    const lookup = el.dataset.replaceFrom;
+    const replace = el.dataset.replaceTo;
+
+    if (text.search(lookup) != -1 && (replace != "" || replace.length != 0)) {
+      el.innerHTML = html.replaceAll(lookup, replace);
+    }
   });
 }
