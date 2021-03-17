3fd2c7809f28bb199db6a18563a343327b0a0499
samueljim <thesamueljim@gmail.com>
2021-03-18 09:23:18 +1000

    updated mto to use team metadata

--- js/mto.js
+++ js/mto.js
@@ -1,109 +1,83 @@
-function setupMTO(mtoList, mtoParams, inputData, additionalData = null) {
-  try {
-    let mtoInputName = mtoParams["inputName"];
-    let formGroupClassList = mtoParams["formGroupClassList"];
-
-    if (mtoInputName == "") {
-      throw "MTO Input Name Not Defined";
-    }
-
-    mtoList = mtoList.split("_").join(" ").toLowerCase();
-
-    var sidebar = getSidebar();
-    var state = getOutfitState();
-    if ((mtoList == "" || mtoList == null) && state != "template") {
-      if (correctInputName(mtoInputName)) {
-        //The user has selected into the MTO input, however some their team is not allowed to use MTO, show an error.
-        showMTOMessage(
-          "MTO is not available for your team. Please click back to continue editing your document.",
-          formGroupClassList
-        );
-      } else {
-        hideInput(mtoInputName);
-      }
-
-      return;
-    }
-
-    if (state == "export" || sidebar == null) {
-      handleMTOData(JSON.parse(inputData), additionalData);
-      return;
-    }
-
-    if (state === "template") {
-      if (correctInputName(mtoInputName)) {
-        showMTOMessage(
-          "MTO functionality is not available on a template level.",
-          formGroupClassList
-        );
-      }
-
-      return;
-    }
-
-    let inputList = sidebar.querySelectorAll(inputListClass);
-    inputList.forEach((inputOption) => {
-      let teamName = inputOption.querySelector("span").innerText.toLowerCase();
-      if (!mtoList.includes(teamName)) {
-        inputOption.remove();
-      }
-    });
-
-    handleMTOData(JSON.parse(inputData), additionalData);
-  } catch (error) {
-    console.error(
-      "An MTO error has occurred. Please try again later. If the issue still persists please contact Outfit Support"
-    );
-    console.error("MTO Error: " + error);
-    return;
+  // get sidebar element on the current page
+  let getSidebar = () => {
+    let sidebar = window.parent.document.querySelectorAll(".sidebar .current-list");
+    return [...sidebar].pop();
   }
-}
-
-function showMTOMessage(message, formGroupClassList) {
+  
+  let showMTOMessage = (message) => {
+    sidebar.querySelector(formGroupClassList).innerHTML = message;
+  }
+  let correctInputName = (name) => {
+    return sidebar.querySelector(".flex.items-center .h-3.ff-open-sans.truncate").innerText === name;
+  }
+  
+  let hideInput = (inputValue) => {
+    sidebar.querySelectorAll(".sidebar-items").find((input) => {
+      input.querySelector(".field-variable-tag").innerText == inputValue
+    }).style.display = 'none';
+  }
+  // gobal varbiles to avoid needed to retreve these more than once 
   var sidebar = getSidebar();
-  var formGroup = sidebar.querySelector(formGroupClassList);
-  var inputList = formGroup.querySelector("div");
-
-  if (inputList != null) {
-    inputList.remove();
-  }
-
-  if (getSidebar().querySelector(`${formGroupClassList} div`) == null) {
-    formGroup.innerHTML = message;
-  }
-}
-
-function getParent() {
-  return window.parent.document;
-}
-
-function getSidebar() {
-  var sidebar = getParent().querySelectorAll(".sidebar .current-list");
-  sidebar = sidebar[sidebar.length - 1];
-  return sidebar;
-}
-
-function correctInputName(expectedInputName) {
-  if (getCurrentInputName() === expectedInputName) {
-    return true;
-  }
-
-  return false;
-}
-
-function getCurrentInputName() {
-  var currentInputName = getSidebar().querySelector(".flex.items-center .h-3.ff-open-sans.truncate").innerText;
-  return currentInputName;
-}
-
-function hideInput(inputValue) {
-  var parentDoc = window.parent.document;
-  var sidebar = parentDoc.querySelector(".sidebar .current-list");
-  var inputs = sidebar.querySelectorAll(".sidebar-items");
-  inputs.forEach(function (input) {
-    var inputName = input.querySelector(".field-variable-tag").innerText;
-    if (inputValue == inputName) {
-      input.style.display = "none";
+  // get the state of the docment in order to tell if it's a template/document/export
+  var state = document.body.getAttribute("document-state");
+  
+  var setupMTO = (mtoList, mtoParams, inputData, settings) => {
+    try {
+      let { inputName, inputListClassList, formGroupClassList } = mtoParams;
+    
+      if (inputName == "") {
+        throw "MTO Input Name Not Defined";
+      }
+   
+      mtoList = mtoList.toLowerCase().split("_").join(" ").split(',');
+  
+      if (!mtoList && state !== "template") {
+        if (correctInputName(inputName)) {
+          // The user has selected into the MTO input, however some their team is not allowed to use MTO, show an error.
+          showMTOMessage("MTO is not available for your team. Please click back to continue editing your document.");
+        } else {
+          hideInput(inputName);
+        }
+        return;
+      }
+  
+      let data = JSON.parse(inputData);
+      
+      if (state === "export" || !sidebar) {
+        if (typeof handleMTOData == 'function')
+          handleMTOData(data, settings);
+        return;
+      }
+  
+      if (state === "template") {
+        if (correctInputName(inputName)) {
+          showMTOMessage(
+            "MTO functionality is not available on a template level."
+          );
+        }
+        return;
+      }
+      window.hideTeamMetadata = () => {
+        getSidebar().querySelectorAll('.search-bar-wrapper').forEach(el => el.remove())
+        getSidebar().querySelectorAll('.action-buttons').forEach(el => el.remove())
+        getSidebar().querySelectorAll(inputListClassList).forEach((inputOption) => {
+          if (!mtoList.includes(inputOption.querySelector("input").value)) {
+            inputOption.remove();
+          } else {
+            let span = inputOption.querySelector("span");
+            if (span.innerText.match(/^[0-9]{4}/g)) {
+              span.innerText = span.innerText.substring(7);
+            }
+          }
+        });
+      }
+      setInterval(() => hideTeamMetadata(), 500)
+      hideTeamMetadata();
+      if (typeof handleMTOData == 'function') 
+        handleMTOData(data, settings);
+    } catch (error) {
+      console.error("An MTO error has occurred. Please try again later. If the issue still persists please contact Outfit Support");
+      console.error("MTO Error: " + error);
+      return;
     }
-  });
-}
+  }
