ad411ba986650f53c4705af1432d49f7a60eba7f
alexpwooddev <alexpwooddev@gmail.com>
2022-11-02 14:56:00 +1000

    remove flaky test

--- modules/test/boilerplate.test.js
+++ modules/test/boilerplate.test.js
@@ -1,71 +1,61 @@
 /* eslint-disable import/no-named-as-default */
 // eslint-disable-next-line import/no-named-as-default-member
-import Boilerplate from '../boilerplate';
+import Boilerplate from "../boilerplate";
 
-test('1 + 2 to equal 3', () => {
+test("1 + 2 to equal 3", () => {
   expect(1 + 2).toBe(3);
 });
-describe('import tests', () => {
-  test('boilerpalte import', () => {
+describe("import tests", () => {
+  test("boilerpalte import", () => {
     const template = new Boilerplate({});
     expect(template);
   });
-  test('boilerpalte import without object', () => {
+  test("boilerpalte import without object", () => {
     const template = new Boilerplate();
     expect(template);
   });
-  test('boilerpalte import without fonts', () => {
+  test("boilerpalte import without fonts", () => {
     const template = new Boilerplate();
     expect(template.fonts).toBe(undefined);
     expect(template.textFit).toBeInstanceOf(Function);
   });
-  test('boilerpalte fonts will be turned into arrays ', () => {
-    const template = new Boilerplate({ fonts: 'PUT_ALL_FONT_NAMES_HERE' });
-    expect(template.fonts).toStrictEqual(['PUT_ALL_FONT_NAMES_HERE']);
+  test("boilerpalte fonts will be turned into arrays ", () => {
+    const template = new Boilerplate({ fonts: "PUT_ALL_FONT_NAMES_HERE" });
+    expect(template.fonts).toStrictEqual(["PUT_ALL_FONT_NAMES_HERE"]);
   });
-  test('boilerpalte fonts can be an array', () => {
-    const template = new Boilerplate({ fonts: ['PUT_ALL_FONT_NAMES_HERE'] });
-    expect(template.fonts).toStrictEqual(['PUT_ALL_FONT_NAMES_HERE']);
+  test("boilerpalte fonts can be an array", () => {
+    const template = new Boilerplate({ fonts: ["PUT_ALL_FONT_NAMES_HERE"] });
+    expect(template.fonts).toStrictEqual(["PUT_ALL_FONT_NAMES_HERE"]);
   });
-  test('boilerpalte blank font arrays ', () => {
+  test("boilerpalte blank font arrays ", () => {
     const template = new Boilerplate({ fonts: [] });
     expect(template.fonts).toStrictEqual([]);
   });
-  test('boilerpalte fonts can be an array of many items', () => {
-    const template = new Boilerplate({ fonts: ['Outfit', 'Bendgio Sans'] });
-    expect(template.fonts).toStrictEqual(['Outfit', 'Bendgio Sans']);
+  test("boilerpalte fonts can be an array of many items", () => {
+    const template = new Boilerplate({ fonts: ["Outfit", "Bendgio Sans"] });
+    expect(template.fonts).toStrictEqual(["Outfit", "Bendgio Sans"]);
   });
-  test('check default values are be set', () => {
+  test("check default values are be set", () => {
     const template = new Boilerplate();
     expect(template.fonts).toStrictEqual(undefined);
     expect(template.ensureImagesLoad).toStrictEqual(true);
     expect(template.exportReduceFont).toStrictEqual(0);
     // expect(template.variables).toStrictEqual({});
   });
-  test('check values can be changed', () => {
+  test("check values can be changed", () => {
     const template = new Boilerplate({
-      fonts: ['Test'],
+      fonts: ["Test"],
       ensureImagesLoad: false,
       addCrop: false,
       exportReduceFont: 0.4,
-      cssVariables: '--plum: red;',
+      cssVariables: "--plum: red;",
       random:
         "this will not be saved as it's not something I know what to do with",
     });
-    expect(template.fonts).toStrictEqual(['Test']);
+    expect(template.fonts).toStrictEqual(["Test"]);
     expect(template.ensureImagesLoad).toStrictEqual(false);
     expect(template.exportReduceFont).toStrictEqual(0.4);
     expect(template.random).toStrictEqual(undefined);
-  });
-  test('check error get thrown if no html is found', async () => {
-    try {
-      // eslint-disable-next-line no-unused-vars
-      const template = new Boilerplate({ variables: { test: 'hey' } });
-    } catch (e) {
-      expect(e).toBe(
-        "No fonts were put in the boilerplate config. For example { fonts: ['IBM Plex Sans'] }",
-      );
-    }
   });
 });
 // I want to test this but I hvaven't worked out how to check console logs TODO
